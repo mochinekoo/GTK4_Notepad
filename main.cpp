4f205ef57487d231dcc4ff2c620783f13f5c29e1
mochinekoo <mochinekoo0113@gmail.com>
2026-09-16 19:50:09 +0900

    [add] メニューバーを出すところまで追加しました

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <gtk/gtk.h>
 
 namespace {
+    const int DEFAULT_WIDTH = 300, DEFAULT_HEIGHT = 300;
     GtkApplication* gtkApplication_ = nullptr;
+    GtkWindow* gtkMainWindow_ = nullptr;
 }
 
 static void activate(GtkApplication* app, gpointer data);
@@ -21,8 +23,24 @@ int main(int argc, char** argv) {
 
 static void activate(GtkApplication* app, gpointer data) {
     GtkWidget* window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "メモ帳");
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 300);
+    gtkMainWindow_ = GTK_WINDOW(window);
+
+    gtk_window_set_title(gtkMainWindow_, "メモ帳");
+    gtk_window_set_default_size(gtkMainWindow_, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+    GtkWidget* vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+
+    GMenu* menu = g_menu_new();
+    GMenu* fileMenu = g_menu_new();
+    g_menu_append(fileMenu, "開く", "menu.openFile");
+    g_menu_append(fileMenu, "閉じる", "menu.close");
+
+    g_menu_append_submenu(menu, "ファイル", G_MENU_MODEL(fileMenu));
+    GtkWidget* menuBar = gtk_popover_menu_bar_new_from_model(G_MENU_MODEL(menu));
+
+    gtk_box_append(GTK_BOX(vbox), menuBar);
+
+    gtk_window_set_child(GTK_WINDOW(window), vbox);
 
     gtk_window_present(GTK_WINDOW(window));
 }
